fb9c5a7effaf73516b6b71ef21d4f4d256a2e5bc
emanuelefarelli <emaaliens888@gmail.com>
2023-07-04 17:07:30 +0200

    Added bootstrap cdn and bootstrap classes

--- index.php
+++ index.php
@@ -44,13 +44,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- BOOTSTRAP 5.2 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+
     <title>Password Generator</title>
 </head>
-<body>
-    <h1>
+<body class="bg-secondary text-light">
+    <h1 class="text-center p-5 bg-dark">
         Very Strong Password Generator!
     </h1>
-    <div>
+    <div class="mx-auto mt-4 p-4 w-50 bg-primary text-center">
         <form method="get">
             <label for="length-input">
                 Lunghezza Password:
@@ -62,18 +67,18 @@
         </form>
     </div>
     <?php if((($_GET['lenght-input']) > 0) && (($_GET['lenght-input'])!= null)){ ?>
-    <div>
+    <div class="bg-info w-50 mx-auto mt-5 p-5">
         <h2>
             La tua password generata è:
         </h2>
-        <p>
+        <p class="fs-2 text-success">
             <?php
                 echo $newPassword;
             ?>
         </p>
     </div>
     <?php } else { ?>
-    <div>
+    <div class="bg-warning w-50 mx-auto mt-5 p-5">
         <p>
             I parametri inseriti non sono validi, prego rincontrollare.
         </p>
