bd8d0e1e22c4e13d373c310638f6dcc2863ea7e4
emanuelefarelli <emaaliens888@gmail.com>
2023-07-04 16:47:49 +0200

    Improved count() to read arrays length

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 
     
     function generatePassword($lenght){
-        $specialArray = ['!','@','#','$','%','^','&','*','(',')','_','-','!','=','?',':','.',';',];
-        $charArray = ['a','b','c','d','e','f','g','k','h','i','l','m','n','o','p','q','r','s','t','u','v','x','y','z',];
+        $specialArray = ['!','@','#','$','%','^','&','*','(',')','_','-','!','=','?',':','.',';'];
+        $charArray = ['a','b','c','d','e','f','g','k','h','i','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
         $numArray = ['1','2','3','4','5','6','7','8','9','0'];
         $newPassword = [];
             
@@ -17,18 +17,18 @@
                 
             if($charType === 1){
 
-                $takeSpecialchar = rand(0,17);
+                $takeSpecialchar = rand(0,(count($specialArray)-1));
                 array_push($newPassword,$specialArray[$takeSpecialchar]);
 
             }elseif($charType === 2){
 
-                $takeChar = rand(0,23);
+                $takeChar = rand(0,(count($charArray)-1));
                 array_push($newPassword,$charArray[$takeChar]);
 
 
             }elseif($charType === 3){
 
-                $takeNumber = rand(0,9);
+                $takeNumber = rand(0,(count($numArray)-1));
                 array_push($newPassword,$numArray[$takeNumber]);
 
             }
